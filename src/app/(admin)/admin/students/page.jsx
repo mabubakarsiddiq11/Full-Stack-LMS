60301372048691c0b57a15fd1629454b2781cd32
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2024-10-25 10:14:40 -0700

    add table in Student Page

--- src/app/(admin)/admin/students/page.jsx
+++ src/app/(admin)/admin/students/page.jsx
@@ -1,9 +1,11 @@
+import { StudentTable } from '@/components/TableData/StudentTable'
 import React from 'react'
 
 function page() {
   return (
-    <div>
+    <div className='container mx-auto'>
       <h1 className='text-5xl font-bold text-center'>Hello Student</h1>
+      <StudentTable/>
     </div>
   )
 }
