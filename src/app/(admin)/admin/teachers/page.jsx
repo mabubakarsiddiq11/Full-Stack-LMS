11431a486c14e29dd13875cfdf3aefdadc26471f
mabubakarsiddiq11 <mabubakarsiddiq11@gmail.com>
2024-10-25 10:09:43 -0700

    Add this Table in Teachers Page

--- src/app/(admin)/admin/teachers/page.jsx
+++ src/app/(admin)/admin/teachers/page.jsx
@@ -1,9 +1,11 @@
+import { TeachersTable } from '@/components/TableData/TeachersTable'
 import React from 'react'
 
 function page() {
   return (
-    <div>
+    <div className='container mx-auto'>
       <h1 className='text-5xl font-bold text-center'>Hello Teacher</h1>
+      <TeachersTable/>
     </div>
   )
 }
